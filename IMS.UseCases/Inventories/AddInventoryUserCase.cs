af548c97dd99eda242a28e5701f0fd98367615df
Skeeter-Man <micheal_newman@yamaha-motor.com>
2024-06-26 12:39:18 -0500

    Add Inventory Use Case

---
+++ IMS.UseCases/Inventories/AddInventoryUserCase.cs
@@ -0,0 +1,25 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases.Inventories
+{
+    public class AddInventoryUserCase : IAddInventoryUserCase
+    {
+        private readonly IInventoryRepository _inventoryRepository;
+
+        public AddInventoryUserCase(IInventoryRepository inventoryRepository)
+        {
+            _inventoryRepository = inventoryRepository;
+        }
+
+        public async Task ExecuteAsync(Inventory inventory)
+        {
+            await _inventoryRepository.AddInventoryAsync(inventory);
+        }
+    }
+}
---
+++ IMS.UseCases/Inventories/Interfaces/IAddInventoryUserCase.cs
@@ -0,0 +1,9 @@
+﻿using IMS.CoreBusiness;
+
+namespace IMS.UseCases.Inventories
+{
+    public interface IAddInventoryUserCase
+    {
+        Task ExecuteAsync(Inventory inventory);
+    }
+}
